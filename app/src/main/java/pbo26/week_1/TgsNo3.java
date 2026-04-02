f57e6c1de41afb153783eff640d0b832fdfab3aa
achmadtifli <tifliachmad@gmail.com>
2026-04-02 00:39:26 -0700

    feat:Tugas Praktikum 1

---
+++ app/src/main/java/pbo26/week_1/TgsNo3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class TgsNo3 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+
+        System.out.print("Masukkan password: ");
+        String password = x.nextLine();
+
+        boolean hurufBesar = false;
+        boolean hurufKecil = false;
+        boolean angka = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                hurufBesar = true;
+            } else if (Character.isLowerCase(c)) {
+                hurufKecil = true;
+            } else if (Character.isDigit(c)) {
+                angka = true;
+            }
+        }
+
+        if (password.length() >= 8 && hurufBesar && hurufKecil && angka) {
+            System.out.println("Password valid");
+        } else {
+            System.out.println("Password tidak valid");
+        }
+    }
+}
---
+++ app/src/main/java/pbo26/week_1/TgsNo4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TgsNo4 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int angka = x.nextInt();
+        int faktorial = 1;
+        for (int i = 1 ; i<= angka; i++) {
+            faktorial *= i;
+        }
+        System.out.println(faktorial);
+    }
+}
